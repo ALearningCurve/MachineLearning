8207226ac534164fde0ec5c845010c73be009135
afkhurana <31321265+afkhurana@users.noreply.github.com>
2020-09-02 21:01:04 -0400

    Moved some stuff around

--- mnist_gan/digit_select/main.py
+++ mnist_gan/digit_select/main.py
@@ -1,155 +1,106 @@
 import tensorflow as tf
 
-import pprint as pp
+import glob
+import imageio
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-from tensorflow import keras
+import PIL
 from tensorflow.keras import layers
+from tensorflow import keras
 import time
 
+from IPython import display
 
 
-BUFFER_SIZE = 60000
-BATCH_SIZE = 256
+models_path = r"C:\Users\cayse\Desktop\WillyWally\models"
 
-EPOCHS = 200
-noise_dim = 500
+generator = keras.models.load_model(os.path.join(models_path, "digit_generator"))
+classifier = keras.models.load_model(os.path.join(models_path, "digit_classifier"))
+
+
+def make_noise_generator_model():
+	model = tf.keras.Sequential()
+	model.add(layers.Dense(300, input_shape=(100,)))
+	model.add(layers.Dropout(0.2))
+	model.add(layers.Dense(300))
+	model.add(layers.Dropout(0.5))
+	model.add(layers.Dense(100))
+	return model
+
+noise_generators = []
+for i in range(10):
+	noise_generators.append(make_noise_generator_model())
+
+
+
+cross_entropy = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+
+def noise_generator_loss(classifier_output, digit_type):
+	return cross_entropy(classifier_output, digit_type)
+
+optimizer = tf.keras.optimizers.Adam(1e-4)
+
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(optimizer=optimizer,
+								model=noise_generator)
+
+
+EPOCHS = 50
+BATCH_SIZE = 1024
+noise_dim = 100
 num_examples_to_generate = 16
 
-set_num = 3
-
-general_generator = keras.models.load_model("digit_generator")
-classifier = keras.models.load_model("digit_classifier")
-discriminator = keras.models.load_model("digit_discriminator")
-
-"""
-def make_generator_model():
-	model = tf.keras.Sequential()
-	model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(100,)))
-	model.add(layers.BatchNormalization())
-	model.add(layers.LeakyReLU())
-
-	model.add(layers.Reshape((7, 7, 256)))
-	assert model.output_shape == (None, 7, 7, 256) # Note: None is the batch size
-
-	model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False, activation=tf.nn.leaky_relu))
-	assert model.output_shape == (None, 7, 7, 128)
-	model.add(layers.BatchNormalization())
-	model.add(layers.LeakyReLU())
-
-	model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation=tf.nn.leaky_relu))
-	assert model.output_shape == (None, 14, 14, 64)
-	model.add(layers.BatchNormalization())
-	model.add(layers.LeakyReLU())
-
-	model.add(layers.Conv2DTranspose(32, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
-	assert model.output_shape == (None, 28, 28, 32)
-
-	model.add(layers.Conv2DTranspose(1, (5, 5), strides=(1, 1), padding='same', use_bias=False, activation='tanh'))
-	assert model.output_shape == (None, 28, 28, 1)
-
-	return model
-"""
-
-def make_generator_model():
-	model = tf.keras.Sequential()
-	model.add(layers.Dense(7*7*256, use_bias=False, input_shape=(noise_dim,)))
-	model.add(layers.BatchNormalization())
-	model.add(layers.LeakyReLU())
-
-	model.add(layers.Reshape((7, 7, 256)))
-	assert model.output_shape == (None, 7, 7, 256) # Note: None is the batch size
-
-	model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False, activation=tf.nn.leaky_relu))
-	assert model.output_shape == (None, 7, 7, 128)
-	model.add(layers.BatchNormalization())
-	model.add(layers.LeakyReLU())
-
-	model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation=tf.nn.leaky_relu))
-	assert model.output_shape == (None, 14, 14, 64)
-	model.add(layers.BatchNormalization())
-	model.add(layers.LeakyReLU())
-
-	model.add(layers.Conv2DTranspose(32, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
-	assert model.output_shape == (None, 28, 28, 32)
-
-	model.add(layers.Conv2DTranspose(1, (5, 5), strides=(1, 1), padding='same', use_bias=False, activation='tanh'))
-	assert model.output_shape == (None, 28, 28, 1)
-
-	return model
-
-
-
-
-digit_generators = []
-for i in range(0, 10):
-	digit_generators.append(make_generator_model())
-
-cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-def generator_loss(fake_output, classifier_guess, generated_digit):
-	discriminator_loss = cross_entropy(tf.ones_like(fake_output), fake_output)
-	indices = [generated_digit for i in range(BATCH_SIZE)]
-	classifier_loss = cross_entropy(tf.one_hot(indices, 10), classifier_guess)
-	total_loss = discriminator_loss + classifier_loss
-	return total_loss
-generator_optimizer = tf.keras.optimizers.Adam(1e-4)
-checkpoint_dirs = []
-checkpoint_prefixes = []
-checkpoints = []
-for digit, generator in enumerate(digit_generators):
-	checkpoint_dir = './training_checkpoints_{}'.format(digit)
-	checkpoint_dirs.append(checkpoint_dir)
-	checkpoint_prefixes.append(os.path.join(checkpoint_dir, "ckpt"))
-	checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
-								 generator=generator)
-	checkpoints.append(checkpoint)
-
+#GIF seed
+seed = tf.random.normal([num_examples_to_generate, noise_dim])
 
 
 
 @tf.function
-def train_step():
+def train_step(digit_type, noise_generator):
 	noise = tf.random.normal([BATCH_SIZE, noise_dim])
 
-	for digit, generator in enumerate(digit_generators):
-		with tf.GradientTape() as gen_tape:
-			generated_images = generator(noise, training=True)
+	with tf.GradientTape() as tape:
+		generated_noise = noise_generator(noise, training=True)
 
-			classifier_guess = classifier(generated_images)
-			fake_output = discriminator(generated_images, training=True)
+		generated_images = generator(generated_noise)
+		classifications = classifier(generated_images)
 
-			gen_loss = generator_loss(fake_output, classifier_guess, digit)
+		loss = noise_generator_loss(classifications, digit_type)
 
-		gradients_of_generator = gen_tape.gradient(gen_loss, generator.trainable_variables)
-		generator_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
+	gradients = tape.gradient(loss, noise_generator.trainable_variables)
+	optimizer.apply_gradients(zip(gradients, noise_generator.trainable_variables))
 
-seed = tf.random.normal([num_examples_to_generate, noise_dim])
 
 def train(epochs):
 	for epoch in range(epochs):
-		print("Starting epoch {}".format(epoch+1))
 		start = time.time()
-		train_step()
-		for digit, generator in enumerate(digit_generators):
-			generate_and_save_images(generator,
-								 epoch + 1,
-								 seed,
-								 digit)
-		if (epoch + 1) % 10 == 0:
-			for digit in range(10):
-				checkpoint = checkpoints[digit]
-				checkpoint_prefix = checkpoint_prefixes[digit]
-				checkpoint.save(file_prefix = checkpoint_prefix)
-		print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
-	for digit, generator in enumerate(digit_generators):
+
+		for i, image_batch in enumerate(dataset, 1):
+			print("Batch {}".format(i))
+			train_step(image_batch)
+
+		# Produce images for the GIF as we go
+		display.clear_output(wait=True)
 		generate_and_save_images(generator,
 							 epoch + 1,
-							 seed,
-							 digit)
+							 seed)
+
+		# Save the model every 15 epochs
+		if (epoch + 1) % 15 == 0:
+			checkpoint.save(file_prefix = checkpoint_prefix)
+
+		print ('Time for epoch {} is {} sec'.format(epoch + 1, time.time()-start))
+
+	# Generate after the final epoch
+	display.clear_output(wait=True)
+	generate_and_save_images(generator,
+						   epochs,
+						   seed)
 
 
-def generate_and_save_images(model, epoch, test_input, digit):
+def generate_and_save_images(model, epoch, test_input):
 	# Notice `training` is set to False.
 	# This is so all layers run in inference mode (batchnorm).
 	predictions = model(test_input, training=False)
@@ -161,12 +112,36 @@ def generate_and_save_images(model, epoch, test_input, digit):
 		plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
 		plt.axis('off')
 
-	if not os.path.exists("img_{}".format(set_num)):
-		os.mkdir("img_{}".format(set_num))
-	plt.savefig('img_{}/{}_image_at_epoch_{:04d}.png'.format(set_num, digit, epoch))
-	plt.close(fig)
+	plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
+	#plt.show()
+
+
+checkpoint.restore(tf.train.latest_checkpoint(checkpoint_dir))
 
 
 
-print("\n\nSTARTING TRAIN")
-train(EPOCHS)
+train(train_dataset, 1)
+
+generator.save_model("digit_generator")
+discriminator.save_model("digit_discriminator")
+
+
+# Display a single image using the epoch number
+def display_image(epoch_no):
+	return PIL.Image.open('image_at_epoch_{:04d}.png'.format(epoch_no))
+
+display_image(EPOCHS)
+
+anim_file = 'dcgan.gif'
+
+with imageio.get_writer(anim_file, mode='I') as writer:
+	filenames = glob.glob('image*.png')
+	filenames = sorted(filenames)
+	for filename in filenames:
+		image = imageio.imread(filename)
+		writer.append_data(image)
+	image = imageio.imread(filename)
+	writer.append_data(image)
+
+import tensorflow_docs.vis.embed as embed
+embed.embed_file(anim_file)
